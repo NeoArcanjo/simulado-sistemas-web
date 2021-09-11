b52676224c1c065c0e8864d9712ff8b8fc387d5e
Sylon <rexmundi.rafael@gmail.com>
2021-09-11 04:22:49 -0300

    Pagina de pokemons concluida

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from typing import Collection
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 # initial app configuration
 app = Flask(__name__)
+
+pages = ["pokemon", "moves", "type"]
 
 """
 A página inicial do aplicativo da web.
@@ -12,46 +13,76 @@ A página inicial do aplicativo da web.
 def index():
     return render_template('base.html')
 
+
 @app.route('/<page>', methods=['GET'])
 def page(page):
+    if page not in pages:
+        return redirect(url_for('index'))
+    
     offset = request.args.get('offset')
-    print(offset)
     limit = request.args.get('limit')
 
     url = f"https://pokeapi.co/api/v2/{page}?offset={offset}&limit={limit}"
 
-    # search = request.args.get('q')
-    # results = requests.get(f"{url}{search}")
-    
     results = requests.get(url)
     results = results.json()
-    if page == "pokemon":
+    if 'results' in results.keys():
         collection = []
         for result in results['results']:
             r = requests.get(result['url'])
             collection.append(r.json())
         results = collection
-    
-    return render_template('%s.html' % page, results=results)
+
+    return render_template('%s.html' % page, results=results, title=page)
 
 # url = "https://pokeapi.co/api/v2/pokemon?offset=" + this.state.offset + "&limit=" + this.state.loadNumber
+
+
 @app.route('/<page>/search', methods=['GET'])
 def search(page):
-    url = f"https://pokeapi.co/api/v2/{page}/"
+    if page not in pages:
+        return redirect(url_for('index'))
 
     search = request.args.get('q')
-    results = requests.get(url + search)
-    print(results.json())
-    return results.json()
+    url = f"https://pokeapi.co/api/v2/{page}/{search}"
+
+    results = requests.get(url)
+    if results.status_code == 200:
+        results = results.json()
+
+        if 'results' in results.keys():
+            collection = []
+            for result in results['results']:
+                r = requests.get(result['url'])
+                collection.append(r.json())
+            results = collection
+
+        if type(results) is list:
+            return render_template('%s.html' % page, results=results, title=page)
+        else:
+            return render_template('about-%s.html' % page, result=results)
+    elif results.status_code == 404:
+        return render_template('not_found.html')
+    else:
+        return render_template('error.html')
 
 
 @app.route('/<page>/<name>/detail', methods=['GET'])
 def about(page, name):
+    if page not in pages:
+        return redirect(url_for('index'))
+
     url = f"https://pokeapi.co/api/v2/{page}/{name}"
 
     results = requests.get(url)
     return render_template('about-%s.html' % page, result=results.json())
 
 
+@app.route('/<path:dummy>')
+def fallback(dummy=None):
+    return redirect(url_for('index'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('not_found.html')
