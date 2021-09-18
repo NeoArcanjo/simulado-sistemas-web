156fc8742f50d00dc544c8b338d0c41c44653d01
Sylon <rexmundi.rafael@gmail.com>
2021-09-17 21:12:19 -0300

    modified:   README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# simulado-sistemas-web
+# Avaliação 1
+
+## Sistemas Web 2021
+
+Prof.: `Marlon Ferrari`
+
+## Projeto:
+
+Em uma concessionária existem os seguintes veículos:
+
+- Fiat Mobi 2021, Zero, valor: 50 mil Reais
+- Renault Sandero 2019, 1.000 KM, valor 35 mil Reais
+- Ferrari Enzo 2009, 100 KM, valor 700 mil Reais
+- Ford Focus 2018, 10.000 KM, 28 mil Reais
+
+Você deverá projetar um Web App capaz de listar os carros com sua quilometragem e valor.
+Para isto:
+
+- [ ] Crie o template para a interface - 3 pontos
+- [ ] Crie o backend com as informações acima (use listas) – 3 pontos
+- [ ] Crie a rotina FrontEnd que percorra e exiba os itens corretamente – 3 pontos
+- [ ] Criatividade e visual – 1 Ponto
+
+Envio: arquivo app.py e o template HTML pelo Classroom APENAS.
--- app.py
+++ app.py
@@ -4,78 +4,17 @@ import requests
 # initial app configuration
 app = Flask(__name__)
 
-pages = ["pokemon", "move", "type"]
-
 """
 A página inicial do aplicativo da web.
 """
+
+
 @app.route('/')
 def index():
-    return render_template('base.html')
+    cars = [{"name": "Fiat Mobi", "ano": 2021, "km": 0, "valor": 50000}, {"name": "Renault Sandero", "ano": 2019, "km": 1000, "valor": 35000},
+            {"name": "Ferrari Enzo", "ano": 2009, "km": 100, "valor": 700000}, {"name": "Ford Focus", "ano": 2018, "km": 10000, "valor": 28000}]
 
-
-@app.route('/<page>', methods=['GET'])
-def page(page):
-    if page not in pages:
-        return redirect(url_for('index'))
-    
-    offset = request.args.get('offset')
-    limit = request.args.get('limit')
-
-    url = f"https://pokeapi.co/api/v2/{page}?offset={offset}&limit={limit}"
-
-    results = requests.get(url)
-    results = results.json()
-    if 'results' in results.keys():
-        collection = []
-        for result in results['results']:
-            r = requests.get(result['url'])
-            collection.append(r.json())
-        results = collection
-
-    return render_template('%s.html' % page, results=results, title=page)
-
-# url = "https://pokeapi.co/api/v2/pokemon?offset=" + this.state.offset + "&limit=" + this.state.loadNumber
-
-
-@app.route('/<page>/search', methods=['GET'])
-def search(page):
-    if page not in pages:
-        return redirect(url_for('index'))
-
-    search = request.args.get('q')
-    url = f"https://pokeapi.co/api/v2/{page}/{search}"
-
-    results = requests.get(url)
-    if results.status_code == 200:
-        results = results.json()
-
-        if 'results' in results.keys():
-            collection = []
-            for result in results['results']:
-                r = requests.get(result['url'])
-                collection.append(r.json())
-            results = collection
-
-        if type(results) is list:
-            return render_template('%s.html' % page, results=results, title=page)
-        else:
-            return render_template('about-%s.html' % page, result=results)
-    elif results.status_code == 404:
-        return render_template('not_found.html')
-    else:
-        return render_template('error.html')
-
-
-@app.route('/<page>/<name>/detail', methods=['GET'])
-def about(page, name):
-    if page not in pages:
-        return redirect(url_for('index'))
-
-    url = f"https://pokeapi.co/api/v2/{page}/{name}"
-
-    results = requests.get(url)
-    return render_template('about-%s.html' % page, result=results.json())
+    return render_template('index.html', results=cars, title="carros")
 
 
 @app.route('/<path:dummy>')
